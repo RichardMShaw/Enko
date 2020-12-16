6e692beb97bf0e5214ee37b3d7e6039d989b4879
Richard Shaw <richardmorganshaw@gmail.com>
2020-12-16 00:00:38 -0800

    renamed discord.js to app.js

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const { token } = require('./keys')
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 require('./db').sync()
   .then(client.login(token);)
