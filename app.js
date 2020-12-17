3d875cccef6a202855d71b18c18d10f4fa518d19
Richard Shaw <richardmorganshaw@gmail.com>
2020-12-17 13:39:54 -0800

    added try and catch

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ const onModMessage = async (msg) => {
 }
 
 client.on('guildMemberUpdate', async (oldMem, newMem) => {
-  let user = await User.findOne({ where: { discordId: newMem.user.id } })
-  user.updateUser(newMem)
-});
+  try {
+    let user = await User.findOne({ where: { discordId: newMem.user.id } })
+    await user.updateUser(newMem)
+  } catch (err) { console.log(err) }
+})
 
 client.on('message', async msg => {
   if (!ready || msg.author.bot) {
@@ -21,17 +23,21 @@ client.on('message', async msg => {
   let { author, channel, content } = msg
   let { id, tag, username } = author
 
-  let user = await User.checkoutGuildMember(await client.guilds.fetch(user.id))
+  try {
+    let user = await User.checkoutGuildMember(await client.guilds.fetch(user.id))
+  } catch (err) { console.log(err) }
 
   console.log(user)
 
-});
+})
 
 client.on('ready', async () => {
-  await User.checkoutGuild(await client.guilds.fetch(serverId))
-  ready = true
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+  try {
+    await User.checkoutGuild(await client.guilds.fetch(serverId))
+    ready = true
+    console.log(`Logged in as ${client.user.tag}!`);
+  } catch (err) { console.log(err) }
+})
 
 require('./db').sync()
   .then(res => {
--- models/User.js
+++ models/User.js
@@ -64,7 +64,9 @@ User.prototype.updateUser = async (guildMember) => {
     self.displayName = (guildMember.nickname) ? guildMember.nickname : guildMember.user.username
   }
 
-  await User.update(self, { where: { discordId: self.discordId } })
+  try {
+    await User.update(self, { where: { discordId: self.discordId } })
+  } catch (err) { console.log(err) }
 
 }
 
@@ -72,32 +74,38 @@ User.prototype.changePoints = function (val) {
   let self = this.dataValues
   console.log(dataValues)
   self.points += val
-  User.update(self, { where: { discordId: self.discordId } })
+  try {
+    await User.update(self, { where: { discordId: self.discordId } })
+  } catch (err) { console.log(err) }
 }
 
 User.prototype.givePoints = (val, guildMember, channel) => {
-  let self = this.dataValues
-  let user = await User.findOne({ where: { discordId: guildMember.user.id } })
-  user = user.dataValues
+  try {
 
-  if (val < 0) {
-    val *= -1
-  }
+    let self = this.dataValues
+    let user = await User.checkoutGuildMember(guildMember)
+    user = user.dataValues
 
-  if (self.points - val < 0) {
-    channel.send(`Not Enough Points!`)
-    return;
-  }
+    if (val < 0) {
+      val *= -1
+    }
 
-  self.points -= val
-  user.points += val
+    if (self.points - val < 0) {
+      await channel.send(`Not Enough Points!`)
+      return;
+    }
 
-  await User.update(self, { where: { discordId: self.discordId } })
-  await User.update(user, { where: { discordId: user.discordId } })
-  await channel.send(
-    `${this.displayName} sent ${val} points to ${user.displayName}.\n`
-    + `${this.displayName}: ${this.points}\n`
-    + `${user.displayName}: ${user.points}`)
+    self.points -= val
+    user.points += val
+
+    await User.update(self, { where: { discordId: self.discordId } })
+    await User.update(user, { where: { discordId: user.discordId } })
+    let s = (val > 1) ? 's' : ''
+    await channel.send(
+      `${this.displayName} gave ${val} point${s} to ${user.displayName}.\n`
+      + `${this.displayName}: ${this.points}\n`
+      + `${user.displayName}: ${user.points}`)
+  } catch (err) { console.log(err) }
 }
 
 module.exports = User
