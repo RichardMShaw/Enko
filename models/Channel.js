cf0ced00bac7447472da4304c558b3c4702a25e2
Richard Shaw <richardmorganshaw@gmail.com>
2020-12-21 19:23:38 -0800

    Created channel model

---
+++ models/Channel.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../db')
+
+class Channel extends Model { }
+
+Channel.init({
+  discordId: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  botCommands: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  },
+  botChat: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  },
+  blacklist: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  }
+}, { sequelize, modelName: 'channel' })
+
+Channel.checkoutGuild = async (guild) => {
+  try {
+    let channels = await guild.channels.cache
+    await channels.forEach(async ({ id, name }) => {
+      name = name.toLowerCase()
+      await Channel.findOrCreate({ where: { name: name }, defaults: { discordId: id, name: name, botCommands: false, botChat: false, blacklist: false } })
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+
+module.exports = Channel
